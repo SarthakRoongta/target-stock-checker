ebf1d123e303f92ee6d3ef5953bf4f5700603936
sarthakroongta <sarthakroongta@gmail.com>
2025-06-22 23:03:54 -0400

    [ios] initial SwiftUI client project

---
+++ ios/TargetStock/TargetStock/TargetStockApp.swift
@@ -0,0 +1,17 @@
+//
+//  TargetStockApp.swift
+//  TargetStock
+//
+//  Created by Sarthak Roongta on 6/22/25.
+//
+
+import SwiftUI
+
+@main
+struct TargetStockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ios/TargetStock/TargetStockTests/TargetStockTests.swift
@@ -0,0 +1,17 @@
+//
+//  TargetStockTests.swift
+//  TargetStockTests
+//
+//  Created by Sarthak Roongta on 6/22/25.
+//
+
+import Testing
+@testable import TargetStock
+
+struct TargetStockTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
